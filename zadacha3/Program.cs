c57fa968698baf591d93e7eab2bf8bcb9b32aec3
Tuyana <tuyaochirova98@gmail.com>
2022-11-23 00:13:11 +0800

    zadacha 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите цифру обозначающую день недели: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i < 1 || i > 7) 
+{
+    Console.WriteLine("Введенная цифра не является днем недели");
+}
+else if (i == 6 || i == 7) 
+{
+    Console.WriteLine("Выходной день");
+}
+else Console.WriteLine("Не выходной день");
