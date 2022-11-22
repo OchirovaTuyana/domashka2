83beac481e04bcd7bb1b0dabf805a499a59fb1b8
Tuyana <tuyaochirova98@gmail.com>
2022-11-22 18:52:08 +0800

    zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+
+int randomNumber = GetRandomNumberInRange(10, 999);
+
+int threeDigitNumber(int number)
+{
+    int intEnd = (randomNumber % 100) / 10;
+    return intEnd;
+}
+
+int maxDigit = threeDigitNumber (randomNumber);
+Console.WriteLine($"Число {randomNumber} третьим числом является {maxDigit}");
+
