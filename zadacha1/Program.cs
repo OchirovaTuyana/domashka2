d4604d9ef2f6fb527c884a2fde620132a065001d
Tuyana <tuyaochirova98@gmail.com>
2022-11-22 16:20:37 +0800

    zadacha 1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+
+int randomNumber = GetRandomNumberInRange(100, 999);
+ 
+int threeDigitNumber = randomNumber;
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine($"Трехзначное число {randomNumber} вторая цифра этого числа является {stringNumber[1]}");
+    
